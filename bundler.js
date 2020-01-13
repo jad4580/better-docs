aab9290195234a3e3757e4a483c233af044bd963
JAD <34970597+jad4580@users.noreply.github.com>
2020-01-13 13:18:15 -0500

    Update bundler.js

--- bundler.js
+++ bundler.js
@@ -61,8 +61,8 @@ module.exports = function bundle (Components, out, config) {
   const entryFile = init + Components.map((c, i) => {
     const { displayName, filePath, type } = c.component
     const relativePath = path.relative(absoluteOut, filePath)
-    console.log('FILE PATH: ' + filePath)
     const name = `Component${i}`
+    console.log('FILE PATH: ' + filePath)
     return [
       `import ${name} from '${filePath.replace(/\\/g, "/")}';`,
       `${type}Components['${displayName}'] = ${name};`,
